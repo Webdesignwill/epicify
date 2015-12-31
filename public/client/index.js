3c69ebc9df7cd2cbb42c21f5df8c636396db7e56
Webdesignwill <will@webdesignwill.co.uk>
2015-12-31 13:47:39 +0100

    Routing abstraction

--- public/client/index.js
+++ public/client/index.js
@@ -1,19 +1,8 @@
 
-import React from 'react';
-import { ReactDOM, render } from 'react-dom';
-import { Router, Route, Link, IndexRoute } from 'react-router';
+import { render } from 'react-dom';
 
-import Application from './containers/Application.js';
-import Login from './pages/login.js';
-import Index from './pages/index.js';
+import Routes from './routes.js';
 
 import "../sass/main.scss";
 
-render((
-  <Router>
-    <Route path="/" component={ Application }>
-      <IndexRoute component={ Index } />
-      <Route path="login" component={ Login }></Route>
-    </Route>
-  </Router>
-), document.querySelector('#application-container'));
+render(( Routes ), document.querySelector('#application-container'));
