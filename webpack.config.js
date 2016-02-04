5d5a45d381523f2e1a7b038658556a315aab3260
Webdesignwill <will@webdesignwill.co.uk>
2016-02-04 20:15:13 +0100

    Small optimisation

--- webpack.config.js
+++ webpack.config.js
@@ -42,11 +42,16 @@ module.exports = {
 
   plugins: debug ? [
     new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
-    new webpack.ProvidePlugin({ $: "jquery", jQuery: "jquery" }),
+    new webpack.ProvidePlugin({ $: "jquery", jQuery: "jquery" })
   ] : [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
   ],
 
   resolve: {
